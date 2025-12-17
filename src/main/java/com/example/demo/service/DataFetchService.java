c4ac519b8553b88e91b5ba01e66556a81c495377
bala-amypo <bala@amypo.in>
2025-12-17 06:27:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DataFetchService.java
@@ -0,0 +1,10 @@
+package com.example.demp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataFetchService{
+    public String fetchData(){
+        return "Welcome to AIDS!!!"
+    }
+}
